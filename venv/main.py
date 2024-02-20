f458abc858f26888422bec2adcce83ede3c6b45e
Kirillov_Ivan <kirillovio@mail.ru>
2024-02-20 20:51:01 +0300

    Импорт функции difference

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,7 @@
-from funcs import sum
+from funcs import sum, difference
 
 print("Введите два числа")
 a = int(input("a-"))
 b = int(input("b-"))
 print(f"Сумма - {sum(a, b)}")
+print(f"Разница - {difference(a, b)}")
